11781954862a6b497d2579f8edbdc8f9513f0cc5
Andrew-Wyn <terraformer.144@gmail.com>
2020-05-11 16:07:41 +0200

    ready for test

--- GRIFFIN/serialdata/SerialHandler.py
+++ GRIFFIN/serialdata/SerialHandler.py
@@ -160,13 +160,13 @@ scanCOMs():
     @classmethod
     def scanCOMs(cls):
         portList = []
-        # for i in range(255):
-        #     try:
-        #         portToCheck = 'COM' + str(i)
-        #         s = serial.Serial(portToCheck)
-        #         s.close()
-        #         portList.append(portToCheck)
-        #     except serial.SerialException:
-        #         pass
-        portList.append("/dev/ttyACM0") # under linux there aren't COM serial
+        for i in range(255):
+            try:
+                portToCheck = 'COM' + str(i)
+                s = serial.Serial(portToCheck)
+                s.close()
+                portList.append(portToCheck)
+            except serial.SerialException:
+                pass
+        # portList.append("/dev/ttyACM0") # under linux there aren't COM serial
         return portList
